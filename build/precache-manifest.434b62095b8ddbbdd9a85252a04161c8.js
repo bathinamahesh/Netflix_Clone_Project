ccac9501028ca9bcda753a60246a9b257b5c5e7a
bathinamahesh <bathinamahesh5399@gmail.com>
2023-04-28 23:47:14 +0530

    first commit

---
+++ build/precache-manifest.434b62095b8ddbbdd9a85252a04161c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7c81d403fb69542f2ab313fac247de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fb8c780e8ca7038271b",
+    "url": "/static/css/main.e24c6389.chunk.css"
+  },
+  {
+    "revision": "924872b8d9ffe8fda2d6",
+    "url": "/static/js/2.0a917c6a.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.0a917c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb8c780e8ca7038271b",
+    "url": "/static/js/main.cfa30429.chunk.js"
+  },
+  {
+    "revision": "7dbe809d8ae504040e68",
+    "url": "/static/js/runtime-main.ebd86647.js"
+  }
+]);
